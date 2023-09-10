df2b97cc43c503c5e04bd6c6d67f124a366f206d
Yaroslava <118363032+YaroslavaBryukhanova@users.noreply.github.com>
2023-09-10 17:02:44 +0300

    Add files via upload

---
+++ week01/ex3.sh
@@ -0,0 +1,19 @@
+cd~/week01
+date
+sleep 3
+mkdir ~/week01/Root
+date
+sleep 3
+cat > ~/week01/Root/root.txt
+date
+sleep 3
+mkdir ~/week01/Home
+date
+sleep 3
+cat > ~/week01/Home/home.txt
+ls / -t -r > ~/week01/Root/root.txt
+ls ~ -t -r > ~/week01/Home/home.txt
+ls ~/week01/Root
+ls ~/week01/Home
+cat ~/week01/Root/root.txt
+cat ~/week01/Home/home.txt
